de7c1c5f4598120f608125ce61f5f68766273d11
aditya.pandey <aditya.pandey@medikabazaar.com>
2024-10-03 15:00:38 +0530

    formatted code

---
+++ src/main/java/com/freemarkertester/service/MyService.java
@@ -0,0 +1,8 @@
+package com.freemarkertester.service;
+
+import java.text.ParseException;
+import java.util.Map;
+
+public interface MyService {
+    Map<String, Object> createOrderAcknowledgement() throws ParseException;
+}
